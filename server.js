d4b8f6cbcbcae114ecbad650ca15ed6041071b5d
jwelleryshowroom <jwelleryshowroom@gmail.com>
2025-10-10 04:48:08 +0530

    Update server.js
    forget to add,

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // ✅ Allow CORS for local dev + deployed frontend
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://jewelleryshowroom.netlify.app' // ✅ your actual Netlify frontend URL
+  'https://jewelleryshowroom.netlify.app',// ✅ your actual Netlify frontend URL
   'https://omjewellersinventory.netlify.app' // client url
 ];
 
